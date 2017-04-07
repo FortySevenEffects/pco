15fed1f552aa4fe91bd989f3a6a3ec811993bf86
Francois Best <francoisbest@gmail.com>
2017-04-07 07:57:15 +0200

    Moved license, changed to MIT.

---
+++ README.md
@@ -0,0 +1,8 @@
+# PCO - Programmatically Controlled Oscillator
+AVR library for generation of pitch-correct waveforms, with effects and modulation support.
+
+## Features
+* Portamento (smooth progression between two pitches)
+* Vibrato (with dedicated LFO)
+* Pulse Width Modulation (for square wave outputs)
+* LFO (generic)
--- pco-controller/pco-controller_PcoModel.h
+++ pco-controller/pco-controller_PcoModel.h
@@ -1,23 +1,3 @@
-/*!
- *  \file       pco-controller_PcoModel.h
- *  \author     Francois Best
- *  \date       15/10/2013
- *  \license    GPL v3.0 - Copyright Forty Seven Effects 2013
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #pragma once
 
 #include "pco-controller.h"
@@ -34,7 +14,7 @@ template<class Traits>
 class PcoModel
 {
 public:
-    typedef typename Traits::SpiInterface   Spi;
+    typedef typename Traits::SpiInterface Spi;
 
 public:
     inline  PcoModel(Spi& inInterface);
